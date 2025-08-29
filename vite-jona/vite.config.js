6cb9314138a7a5e5af3cf366868c895d3497667b
Joonacius <callmejoona@gmail.com>
2025-08-29 11:28:18 -0300

    Prueba 2

--- vite-jona/vite.config.js
+++ vite-jona/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Portfolio-Jona/', // 👈 importante para GitHub Pages
+  base: '/Portfolio-Jona/', // El nombre de tu repositorio
 })
